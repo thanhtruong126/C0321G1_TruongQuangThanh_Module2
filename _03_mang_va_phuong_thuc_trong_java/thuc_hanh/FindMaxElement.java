37dd07b5eed1190c40c75fa980bbc7d400335eef
thanhtruong126 <kirbydn95@gmail.com>
2021-05-05 10:44:41 +0700

    thuc hanh bai 3 mang va phuong thuc trong java

---
+++ _03_mang_va_phuong_thuc_trong_java/thuc_hanh/FindMaxElement.java
@@ -0,0 +1,41 @@
+package bai3_mang_va_phuong_thuc_trong_java.thuc_hanh;
+
+import java.util.Scanner;
+
+public class FindMaxElement {
+    public static void main(String[] args) {
+        // Khai báo biến, nhập và kiểm tra kích thước mảng
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+        // Nhập giá trị cho các phần tử của mảng
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element" + (i + 1) + " : ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        // In ra danh sách đã nhập
+        System.out.print("Property list: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+        }
+        // Duyệt các phần tử trong mảng để tìm GTLN và vị trí
+        int max = array[0];
+        int index = 1;
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] > max) {
+                max = array[j];
+                index = j + 1;
+            }
+        }
+        System.out.println("The largest property value in the list is " + max + " ,at position " + index);
+    }
+}
