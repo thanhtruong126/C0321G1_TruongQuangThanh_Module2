6bebd21f0cecd19f08c8ee18a8e7318264cd9b40
thanhtruong126 <kirbydn95@gmail.com>
2021-05-06 11:06:47 +0700

    thuc hanh bai 3 mang va phuong thuc trong java

---
+++ _03_mang_va_phuong_thuc_trong_java/thuc_hanh/DemSoSinhVienThiDo.java
@@ -0,0 +1,35 @@
+package bai3_mang_va_phuong_thuc_trong_java.thuc_hanh;
+
+import java.util.Scanner;
+
+public class DemSoSinhVienThiDo {
+    public static void main(String[] args) {
+        // Khai báo biến, nhập và kiểm tra kích thước mảng
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+        do {
+            System.out.print("Enter a size:");
+            size = scanner.nextInt();
+            if (size > 30)
+                System.out.println("Size should not exceed 30");
+        } while (size > 30);
+        // Nhập các phần tử của mảng
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter a mark for student " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
+        // Sử dụng vòng lặp for để in ra danh sách điểm vừa nhập, đồng thời đếm số lượng sinh viên thi đỗ
+        int count = 0;
+        System.out.print("List of mark: ");
+        for (int j = 0; j < array.length; j++) {
+            System.out.print(array[j] + "\t");
+            if (array[j] >= 5 && array[j] <= 10)
+                count++;
+        }
+        System.out.print("\n The number of students passing the exam is " + count);
+    }
+}
