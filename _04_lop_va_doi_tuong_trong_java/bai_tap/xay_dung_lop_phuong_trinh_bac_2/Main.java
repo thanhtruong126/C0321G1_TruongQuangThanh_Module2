cffbfb219d997b61209df3cda0b70c052d1c2652
thanhtruong126 <kirbydn95@gmail.com>
2021-05-06 17:34:56 +0700

    bai tap bai 4 lop va doi tuong trong java

--- _04_lop_va_doi_tuong_trong_java/bai_tap/xay_dung_lop_phuong_trinh_bac_2/Main.java
+++ _04_lop_va_doi_tuong_trong_java/bai_tap/xay_dung_lop_phuong_trinh_bac_2/Main.java
@@ -14,7 +14,7 @@ public class Main {
 
         QuadraticEquation quadraticEquation = new QuadraticEquation(a, b, c);
         double delta = quadraticEquation.getDiscriminant();
-        if (delta > 0) {
+        if (delta > 0 && a != 0) {
             System.out.print("Phương trình có 2 nghiệm: " + quadraticEquation.getRoot1() + " và " + quadraticEquation.getRoot2());
         } else if (delta == 0) {
             System.out.print("Phương trình có nghiệm duy nhất: " + quadraticEquation.getRoot2());
