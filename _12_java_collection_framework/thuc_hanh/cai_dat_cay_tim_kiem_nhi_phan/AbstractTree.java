61afa000db06dccd5169eb675a77647f7e53c194
thanhtruong126 <kirbydn95@gmail.com>
2021-05-20 06:53:59 +0700

    thuc hanh bai 12 java collection framework

---
+++ _12_java_collection_framework/thuc_hanh/cai_dat_cay_tim_kiem_nhi_phan/AbstractTree.java
@@ -0,0 +1,7 @@
+package bai12_java_collection_framework.thuc_hanh.cai_dat_cay_tim_kiem_nhi_phan;
+
+public abstract class AbstractTree<E> implements Tree<E> {
+    @Override
+    public void inorder() {
+    }
+}
